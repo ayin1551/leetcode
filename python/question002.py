45930795124c57d4ba7b2044bd856a01a1f3106e
ayin1551 <35798894+ruogao@users.noreply.github.com>
2022-07-03 21:35:53 +0800

    Update question002.py

---
+++ python/question002.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        res = ListNode(0)
+        cursor = res
+        carry = 0
+        while l1 or l2:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            sums = carry + val1 + val2
+            carry = sums // 10
+            cursor.next = ListNode(sums % 10)
+            cursor = cursor.next
+            if l1 is not None:
+                l1 = l1.next
+            if l2 is not None:
+                l2 = l2.next
+        if carry > 0:
+            cursor.next = ListNode(1)
+        return res.next
